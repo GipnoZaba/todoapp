5915a1732f94046c5ff89c6ef6d36a7ecb941c66
Aleksei Beljajev <alekbeljak@gmail.com>
2020-02-12 11:55:29 +0200

    Updates

---
+++ precache-manifest.ca32af87382d3f7fb19e03119d274f57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f46c128c837d4d580282372be5362cf",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "ec5d19c5a11b4b43c079",
+    "url": "/todoapp/static/css/main.7dce5801.chunk.css"
+  },
+  {
+    "revision": "91c21208b4241d1b4259",
+    "url": "/todoapp/static/js/2.7a0d6961.chunk.js"
+  },
+  {
+    "revision": "c0d1569b2748db0ab52e686ed719bcb0",
+    "url": "/todoapp/static/js/2.7a0d6961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5d19c5a11b4b43c079",
+    "url": "/todoapp/static/js/main.5a04ee36.chunk.js"
+  },
+  {
+    "revision": "ad83bdba074762dd371e",
+    "url": "/todoapp/static/js/runtime-main.28cabb72.js"
+  }
+]);
